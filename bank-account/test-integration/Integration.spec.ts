21acc8f11512e0dae2532da7faf080f0afb76567
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2020-07-20 19:44:51 +0100

    ensure customers can not create accounts

--- bank-account/test-integration/Integration.spec.ts
+++ bank-account/test-integration/Integration.spec.ts
@@ -1,4 +1,4 @@
-import { createPassword, createUser, deploy, getAuthToken, graphQLClient, nuke } from './Utils'
+import { createPassword, createUser, getAuthToken, graphQLClient } from './Utils'
 import { UUID } from '@boostercloud/framework-types/dist'
 import { ApolloClient, gql, MutationOptions, NormalizedCacheObject } from '@apollo/client'
 import { expect } from './Setup.spec'
@@ -7,10 +7,13 @@ import * as faker from 'faker'
 describe('Acceptance tests', async () => {
   const bankTellerEmail = faker.internet.email()
   const bankTellerPassword = createPassword()
-  const clientID = UUID.generate()
+  const customerEmail = faker.internet.email()
+  const customerPassword = createPassword()
+  const customerId = UUID.generate()
 
   let unauthorizedClient: ApolloClient<NormalizedCacheObject>
   let bankTellerClient: ApolloClient<NormalizedCacheObject>
+  let customerClient: ApolloClient<NormalizedCacheObject>
   const createBankAccount = (ownerID: UUID): MutationOptions => {
     return {
       mutation: gql`
@@ -28,9 +31,14 @@ describe('Acceptance tests', async () => {
   before(async () => {
     try {
       //deploy()
-      await createUser(bankTellerEmail, bankTellerPassword, 'BankTeller')
+      await Promise.all([
+        createUser(bankTellerEmail, bankTellerPassword, 'BankTeller'),
+        createUser(customerEmail, customerPassword, 'Customer'),
+      ])
+
       unauthorizedClient = await graphQLClient()
       bankTellerClient = await graphQLClient(await getAuthToken(bankTellerEmail, bankTellerPassword))
+      customerClient = await graphQLClient(await getAuthToken(customerEmail, customerPassword))
     } catch (error) {
       throw Error(error)
     }
@@ -42,11 +50,15 @@ describe('Acceptance tests', async () => {
 
   context('CreateBankAccount', () => {
     it('success when submitted by a BankTeller', async () => {
-      const result = await bankTellerClient.mutate(createBankAccount(clientID))
+      const result = await bankTellerClient.mutate(createBankAccount(customerId))
 
       expect(result).to.be.deep.equal({ data: { CreateBankAccount: true } })
     })
-    it.skip('fails when submitted by clients')
+    it('fails when submitted by customers', async () => {
+      const result = customerClient.mutate(createBankAccount(customerId))
+
+      await expect(result).to.eventually.be.rejectedWith("Access denied for command 'CreateBankAccount'")
+    })
     it('fails when submitted without token', async () => {
       const anyOwnerId = UUID.generate()
 
