831a49e1c15698cebbcb4d7f3bebad52b8f9e265
Johan Sebastian Cortes Montenegro <johan@theagilemonkeys.com>
2020-07-20 19:32:05 +0100

    rename client as customer

--- bank-account/src/Roles.ts
+++ bank-account/src/Roles.ts
@@ -8,4 +8,4 @@ export class BankTeller {}
 @Role({
   allowSelfSignUp: true,
 })
-export class Client {}
+export class Customer {}
--- bank-account/src/commands/Deposit.ts
+++ bank-account/src/commands/Deposit.ts
@@ -1,10 +1,10 @@
 import { Register, UUID } from '@boostercloud/framework-types'
 import { DepositPerformed } from '../events/DepositPerformed'
 import { Command } from '@boostercloud/framework-core'
-import { BankTeller, Client } from '../Roles'
+import { BankTeller, Customer } from '../Roles'
 
 @Command({
-  authorize: [Client, BankTeller],
+  authorize: [Customer, BankTeller],
 })
 export class Deposit {
   constructor(readonly iban: UUID, readonly amount: number) {}
