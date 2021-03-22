df11ab34b9a9e07effebc359ac76fd9f38b7cfbf
alvaroloes <alvaro@booster.cloud>
2021-03-22 17:15:31 +0000

    Change environment name

--- asker/backend/src/config/config.ts
+++ asker/backend/src/config/config.ts
@@ -2,7 +2,7 @@ import { Booster } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
 import { Provider } from '@boostercloud/framework-provider-aws'
 
-Booster.configure('production', (config: BoosterConfig): void => {
+Booster.configure('on-aws', (config: BoosterConfig): void => {
   config.appName = 'asker'
   config.provider = Provider()
 })
