9a6ebce944befd39c53b72eed46a002951f777f0
Nikita Tchayka <nick@booster.cloud>
2021-03-24 11:43:15 +0000

    Add k8s support

--- asker/backend/src/config/config.ts
+++ asker/backend/src/config/config.ts
@@ -1,8 +1,14 @@
 import { Booster } from '@boostercloud/framework-core'
 import { BoosterConfig } from '@boostercloud/framework-types'
-import { Provider } from '@boostercloud/framework-provider-aws'
+import * as AWS from '@boostercloud/framework-provider-aws'
+import * as K8s from '@boostercloud/framework-provider-kubernetes'
 
 Booster.configure('on-aws', (config: BoosterConfig): void => {
   config.appName = 'asker'
-  config.provider = Provider()
+  config.provider = AWS.Provider()
+})
+
+Booster.configure('on-k8s', (config: BoosterConfig): void => {
+  config.appName = 'asker'
+  config.provider = K8s.Provider()
 })
