5d1b25d94a9c9a01a06f9dd1b1d3bee64599fb20
David Verdú <davidverdu@theagilemonkeys.com>
2021-03-25 13:05:55 +0000

    command to debug. Split event handler

--- asker-analytics/backend/src/event-handlers/split-questions-into-words.ts
+++ asker-analytics/backend/src/event-handlers/split-questions-into-words.ts
@@ -1,8 +1,14 @@
 import { QuestionCreated } from '../events/question-created'
+import { WordPicked } from '../events/word-picked'
 import { EventHandler } from '@boostercloud/framework-core'
-import { Register } from '@boostercloud/framework-types'
+import { Register, UUID } from '@boostercloud/framework-types'
 
 @EventHandler(QuestionCreated)
 export class SplitQuestionsIntoWords {
-  public static async handle(event: QuestionCreated, register: Register): Promise<void> {}
+  public static async handle(event: QuestionCreated, register: Register): Promise<void> {
+    const words = event.text.split(' ')
+    for (const word in words) {
+      register.events(new WordPicked(word, UUID.generate(), event.id, event.conferenceId))
+    }
+  }
 }
