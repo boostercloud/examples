5e46c37593bb798e94f4c98ec82058a4ca021209
Carlos Dominguez Padron <carlos@theagilemonkeys.com>
2021-03-26 07:20:15 +0000

    added producer (#13)

--- asker/backend/src/config/config.ts
+++ asker/backend/src/config/config.ts
@@ -3,6 +3,28 @@ import { BoosterConfig } from '@boostercloud/framework-types'
 import { Provider } from '@boostercloud/framework-provider-aws'
 
 Booster.configure('on-aws', (config: BoosterConfig): void => {
-  config.appName = 'asker'
-  config.provider = Provider()
+  config.appName = 'asker-demo-charlie'
+  config.provider = Provider([
+    {
+      packageName: '@boostercloud/rocket-kakfa-connector-aws-infrastructure',
+      parameters: {
+        consumerConfig: [],
+        producerConfig: [
+          {
+            topicName: 'asker-questions',
+            eventTypeName: 'QuestionAsked',
+            fields: {
+              questionId: 'questionId',
+              conference: 'conferenceId',
+              text: 'text'
+            }
+          },
+        ],
+        bootstrapServers: [
+          'fast-caboose-01.srvs.cloudkafka.com:9094',
+        ],
+        secretArn: process.env.secretArn
+      },
+    },
+  ])
 })
