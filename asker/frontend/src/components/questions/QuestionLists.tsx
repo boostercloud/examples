81b4357ffe362a5d7a8589aa260a538ef89c3e51
charlietfe <carlos@theagilemonkeys.com>
2021-03-26 16:12:21 +0000

    kafka integration

--- asker/frontend/src/components/questions/QuestionLists.tsx
+++ asker/frontend/src/components/questions/QuestionLists.tsx
@@ -1,10 +1,11 @@
 
 import { Box, makeStyles, Typography, Tooltip, Avatar, Divider, IconButton } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { getInitials, colors } from '../../common/helpers';
-import StarsIcon from '@material-ui/icons/Stars';
 import { useMutation } from '@apollo/client';
 import { CLAP_QUESTION } from '../../common/graphql-queries';
+import { Fade } from "react-awesome-reveal";
+
 
 const useStyles = makeStyles(theme => ({
   postedBy: {
@@ -12,11 +13,13 @@ const useStyles = makeStyles(theme => ({
     fontWeight: 600,
   },
   postedAt: {
-    fontSize: 12,
+    fontSize: 14,
+    fontWeight: 600,
   },
   question: {
-    fontSize: 16,
-    fontWeight: 600,
+    fontStyle: 'light',
+    fontSize: 26,
+    fontWeight: 100,
   },
   questionTitle: {
     marginTop: 50,
@@ -37,12 +40,18 @@ type QuestionItemProps = {
 }
 
 const QuestionItem = (props: QuestionItemProps) => {
-  const { question, index } = props
   const classes = useStyles()
+  const [question, setQuestion] = useState(props.question)
 
   const [Clap] = useMutation(CLAP_QUESTION);
 
   const onCountChange = () => {
+
+    setQuestion({
+      ...question, 
+      claps: question.claps + 1,
+    })
+
     Clap({
       variables: {
         byWhom: localStorage.getItem('email'),
@@ -56,7 +65,7 @@ const QuestionItem = (props: QuestionItemProps) => {
       <Box key={question.id} mt={4} mb={4}>
         <Box display='flex' alignItems='center'>
           <Tooltip title={question.questioner} aria-label='add'>
-            <Avatar style={{ backgroundColor: colors[index % colors.length] }}>{getInitials(question.questioner)}</Avatar>
+            <Avatar style={{ backgroundColor: colors[0] }}>{getInitials(question.questioner)}</Avatar>
           </Tooltip>
           <Box alignSelf='center' flexGrow='1' ml={2}>
             <Typography color='textPrimary' className={classes.postedBy}>
@@ -66,11 +75,11 @@ const QuestionItem = (props: QuestionItemProps) => {
               {new Date().toISOString()}
             </Typography>
           </Box>
-          <Box display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
+          <Box paddingRight={2} display='flex' flexDirection='column' justifyContent='center' alignItems='center'>
             <IconButton aria-label="delete" onClick={onCountChange}>
-              <StarsIcon fontSize='large' color='secondary' />
+              👏
             </IconButton>
-            {question.claps > 0 && <Typography color='textSecondary' className={classes.postedAt}>
+            {question.claps > 0 && <Typography color='secondary' className={classes.postedAt}>
               {question.claps}
             </Typography>}
           </Box>
@@ -93,12 +102,15 @@ type QuestionListProps = {
 export const QuestionsList = (props: QuestionListProps) => {
   const { questions } = props;
   const classes = useStyles()
+
   return (<>
     <Typography className={classes.questionTitle} variant='h6' color='secondary'>
       Questions:
     </Typography>
     { questions.map((item, index) => {
-      return <QuestionItem key={item.id} question={item} index={index} />
+      return <Fade delay={200}>
+        <QuestionItem key={item.id} question={item} index={index} />
+      </Fade>
     })}
   </>)
 };
--- asker/frontend/src/components/questions/QuestionsView.tsx
+++ asker/frontend/src/components/questions/QuestionsView.tsx
@@ -1,7 +1,7 @@
-import { Box, makeStyles } from '@material-ui/core';
-import React, { useEffect } from 'react';
-import { useMutation, useQuery } from '@apollo/client';
-import { CREATE_CONFERENCE, GET_CONFERENCE } from '../../common/graphql-queries';
+import { Box, CircularProgress, makeStyles } from '@material-ui/core';
+import React from 'react';
+import {  useQuery } from '@apollo/client';
+import {  GET_CONFERENCE } from '../../common/graphql-queries';
 import { QuestionsForm } from './QuestionForm';
 import { QuestionsList } from './QuestionLists';
 
@@ -12,42 +12,43 @@ const useStyles = makeStyles(theme => ({
     justifyContent: 'space-around',
     justifySelf: 'center',
     flexDirection: 'row',
-    height: '100vh',
+    marginTop: 100,
+    marginBottom: 100,
+    borderRadius: 8,
   },
   wrapper: {
-    width: '80%',
+    width: '100%',
+  },
+  list: {
+    height: 800,
+    overflow: 'auto'
   }
 }))
 
 export const QuestionsView = () => {
   const classes = useStyles();
-  const [CreateConference, createConferenceResult] = useMutation(CREATE_CONFERENCE);
 
   const queryConferenceResult = useQuery(GET_CONFERENCE, {
     variables: {
       id: localStorage.getItem('conference')
-    }
+    }, 
+    pollInterval: 500
   });
-
-  useEffect(() => {
-    CreateConference({
-      variables: {
-        location: localStorage.getItem('location'),
-        name: localStorage.getItem('conference')
-      }
-    }).then(() => console.log('Creating')).catch(e => console.log(e))
-  }, [CreateConference]);
-
   
-  if (queryConferenceResult.loading || createConferenceResult.loading) {
-    return <p>Loading....</p>
-  }
-
   return (<Box className={classes.root}>
     <Box className={classes.wrapper}>
       <Box display='flex' flexDirection='column' padding={10}>
         <QuestionsForm />
-        <QuestionsList questions={queryConferenceResult.data?.ConferenceReadModel?.questions || []}/>
+
+        {queryConferenceResult?.loading &&
+          <Box alignSelf='center' marginTop={5}>
+            <CircularProgress color="secondary" />
+          </Box>
+        }
+      
+        <Box className={classes.list}>
+          <QuestionsList questions={queryConferenceResult.data?.ConferenceReadModel?.questions || []}/>
+        </Box>
       </Box>
     </Box>
   </Box>
