830c2605c4640aef9d7c1cd0b4f31b6b7ead3373
David Verdú <davidverdu@theagilemonkeys.com>
2021-03-26 11:34:15 +0000

    add kafka rocket

--- asker-analytics/backend/src/config/config.ts
+++ asker-analytics/backend/src/config/config.ts
@@ -3,6 +3,34 @@ import { BoosterConfig } from '@boostercloud/framework-types'
 import { Provider } from '@boostercloud/framework-provider-aws'
 
 Booster.configure('production', (config: BoosterConfig): void => {
-  config.appName = 'asker-analytics'
-  config.provider = Provider()
+  config.appName = 'asker-analytics-davidverdu'
+  config.provider = Provider([
+    {
+      packageName: '@boostercloud/rocket-kakfa-connector-aws-infrastructure',
+      parameters: {
+        consumerConfig: [
+          {
+            topicName: 'asker-questions',
+            mappingOptions: [
+              {
+                entityTypeName: 'Conference',
+                eventTypeName: 'QuestionCreated',
+                topicEntityId: 'conferenceId',
+                fields: {
+                  questionId: 'id',
+                  conferenceId: 'conferenceId',
+                  text: 'text',
+                },
+              },
+            ],
+          },
+        ],
+        producerConfig: [],
+        bootstrapServers: [
+          'fast-caboose-01.srvs.cloudkafka.com:9094',
+        ],
+        secretArn: process.env['KAFKA_SECRET_ARN'],
+      },
+    },
+  ])
 })
--- asker-analytics/backend/src/entities/conference.ts
+++ asker-analytics/backend/src/entities/conference.ts
@@ -1,16 +1,16 @@
 import { Entity, Reduces } from '@boostercloud/framework-core'
 import { UUID } from '@boostercloud/framework-types'
-//import { QuestionCreated } from '../events/question-created'
+import { QuestionCreated } from '../events/question-created'
 import { WordPicked } from '../events/word-picked'
 
 @Entity
 export class Conference {
   public constructor(public id: UUID) {}
 
-  // @Reduces(QuestionCreated)
-  // public static reduceQuestionCreated(event: QuestionCreated, _currentConference?: Conference): Conference {
-  //   return new Conference(event.conferenceId)
-  // }
+  @Reduces(QuestionCreated)
+  public static reduceQuestionCreated(event: QuestionCreated, _currentConference?: Conference): Conference {
+    return new Conference(event.conferenceId)
+  }
 
   @Reduces(WordPicked)
   public static reduceWordPicked(event: WordPicked, _currentConference?: Conference): Conference {
