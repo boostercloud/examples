a97d9174a840e0ba897aaa4cb5b99ea3adc2debe
David Verdú <davidverdu@theagilemonkeys.com>
2021-03-26 15:04:21 +0000

    fix bad rebase

--- asker-analytics/backend/src/event-handlers/split-questions-into-words.ts
+++ asker-analytics/backend/src/event-handlers/split-questions-into-words.ts
@@ -6,20 +6,11 @@ import { Register, UUID } from '@boostercloud/framework-types'
 @EventHandler(QuestionCreated)
 export class SplitQuestionsIntoWords {
   public static async handle(event: QuestionCreated, register: Register): Promise<void> {
-<<<<<<< HEAD
-<<<<<<< HEAD
     const words = event.text
       .replace(/[^0-9a-zA-Z\s]/gi, '')
       .toLowerCase()
       .split(' ')
     const events = words.map((w) => new WordPicked(w, UUID.generate(), event.id, event.conferenceId))
-=======
-    const words = event.text.replace('?','').split(' ')
-=======
-    const words = event.text.split(' ')
->>>>>>> undone change to event handler
-    const events = words.map(w => new WordPicked(w, UUID.generate(), event.id, event.conferenceId))
->>>>>>> enhance event handler
     register.events(...events)
   }
 }
