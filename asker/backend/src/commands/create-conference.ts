d7d64af563b4e1d24744ed0f7535840153b136e8
alvaroloes <alvaro@booster.cloud>
2021-03-29 15:26:20 +0100

    Create conference and questions in one script

--- asker/backend/src/commands/create-conference.ts
+++ asker/backend/src/commands/create-conference.ts
@@ -10,6 +10,9 @@ export class CreateConference {
   public constructor(readonly name: string, readonly location: string) {}
 
   public static async handle(command: CreateConference, register: Register): Promise<void> {
+    if (!command.name) {
+      throw new Error('A conference name is required. Field "name" is empty')
+    }
     const conference = await Booster.entity(Conference, command.name)
     if (conference) {
       throw new Error(`There is already a conference called ${command.name}. Please use a different name`)
