bd0f7737e1fcfeaaef1d695091e0683d1131c2ea
alvaroloes <alvaro@booster.cloud>
2021-04-07 13:11:35 +0100

    Update the final like-questions test

--- askme/backend/src/config/config.ts
+++ askme/backend/src/config/config.ts
@@ -1,15 +1,17 @@
 import { Booster } from '@boostercloud/framework-core'
-import { BoosterConfig } from '@boostercloud/framework-types'
+import { BoosterConfig, Level } from '@boostercloud/framework-types'
 import { Provider as ProviderAWS } from '@boostercloud/framework-provider-aws'
 import { Provider as ProviderK8s } from '@boostercloud/framework-provider-kubernetes'
 import { Provider as ProviderAzure } from '@boostercloud/framework-provider-azure'
 import { BoosterK8sConfiguration } from '@boostercloud/framework-provider-kubernetes-infrastructure'
+
 require('dotenv').config()
 
 Booster.configure('on-aws', (config: BoosterConfig): void => {
   config.appName = process.env.appName ?? 'askme'
   config.assets = ['.env']
   config.provider = ProviderAWS([])
+  config.logLevel = Level.error
 })
 
 Booster.configure('on-azure', (config: BoosterConfig): void => {
